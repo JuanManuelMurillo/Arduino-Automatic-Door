19b43415238765b143f11910ec6b0d20040442b3
JuanManuelMurillo <125754385+JuanManuelMurillo@users.noreply.github.com>
2023-02-18 21:47:34 -0600

    Update Automatic Door Ultrasonic Sensor.ino

--- Automatic Door Ultrasonic Sensor.ino
+++ Automatic Door Ultrasonic Sensor.ino
@@ -1,34 +1,38 @@
-const int Trigger = 2;
-const int Echo = 3;
-const int greenled = 9;
-const int redled = 10;
+const int Trigger = 2; // Define Trigger pin
+const int Echo = 3; // Define Echo pin
+const int greenled = 9; // Define greenled pin
+const int redled = 10; // Define redled pin
 #include <Servo.h>
-Servo servo1;
-Servo servo2;
+Servo servo1; // Add a servo object
+Servo servo2; // Add a second servo object
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); // Initialize Serial monitor 
   pinMode(Trigger, OUTPUT);
   pinMode(Echo, INPUT);
   digitalWrite(Trigger, LOW);
   pinMode(greenled, OUTPUT);
   pinMode(redled, OUTPUT);
 
-servo1.attach(5);
+  // attach servo objects to pins 5,6
+servo1.attach(5); 
 servo2.attach(6);
 
+  // Set servos at 0
 servo1.write(0);
 servo2.write(0);
 
+  // Turn off LEDs
 digitalWrite(greenled, LOW);
 digitalWrite(redled, LOW);
 }
 
 void loop() {
 
+  // Add variables "t" for time and "d" for distance
 long t;
 long d;
-
+  
 digitalWrite(Trigger, HIGH);
   delayMicroseconds(60);
   digitalWrite(Trigger, LOW);
@@ -37,7 +41,7 @@ digitalWrite(Trigger, HIGH);
 
   Serial.println(d);
 
- if (d <50){
+ if (d <50){ // Code runs if the distance is lower than 50cm
     digitalWrite(greenled, HIGH);
     servo1.write(90);
     servo2.write(90);
